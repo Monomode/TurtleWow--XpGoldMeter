0bbc820126a48e907851a0c772c03e5f93652840
Monomode <46225792+Monomode@users.noreply.github.com>
2025-09-30 22:34:14 -0400

    Update XpGoldMeter.lua

--- XpGoldMeter.lua
+++ XpGoldMeter.lua
@@ -8,6 +8,16 @@ local elapsed = time() - startTime
 local gainedXP = UnitXP("player") - startXP
 local xpPerHour = (gainedXP / elapsed) * 3600
 
+-- Gold/hr
+local gainedGold = GetMoney() - startGold
+local gold = floor(gainedGold / 10000)
+local silver = floor((gainedGold % 10000) / 100)
+local copper = gainedGold % 100
+local goldPerHour = (gainedGold / elapsed) * 3600
+local gph = floor(goldPerHour / 10000)
+local sph = floor((goldPerHour % 10000) / 100)
+local cph = goldPerHour % 100
+
 local frame = CreateFrame("Frame", "XpGoldOverlay", UIParent)
 frame:ClearAllPoints()
 frame:SetWidth(115)
